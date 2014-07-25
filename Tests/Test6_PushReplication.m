6e1090a49215920f6f8e0a67959b5cfea92667d7
Li Yang <li@couchbase.com>
2014-07-24 17:42:36 -0700

    Removed files checked in by mistake

--- Tests/Test6_PushReplication.m
+++ Tests/Test6_PushReplication.m
@@ -10,14 +10,13 @@
 #import "Test6_PushReplication.h"
 #import <CouchbaseLite/CouchbaseLite.h>
 
-#define kNumberOfDocuments 25000
-// size in bytes
-#define kSizeofDocument 10000
-
-
 @implementation Test6_PushReplication
 {
-    NSDate* _pushStartTime;
+    NSDate* start;
+    NSString *syncGatewayUrl;
+    int kNumberOfDocuments;
+    int kSizeofDocument;
+    double kpiTotalTime;
 }
 
 - (void) replicationChanged: (NSNotificationCenter*)n {
@@ -33,57 +32,66 @@
    if (self.push.status == kCBLReplicationStopped && !self.push.lastError) {
        [self logFormat: @"Replication Stopped and No Error Found"];
        NSDate *methodFinish = [NSDate date];
-       NSTimeInterval executionTime = [methodFinish timeIntervalSinceDate:_pushStartTime];
+       NSTimeInterval executionTime = [methodFinish timeIntervalSinceDate:start];
        [self logFormat:@"Total Time Taken: %f",executionTime];
        [self logFormat: @"Completed %d Out of total %d",completed,total];
    }
    
    if (self.push.status == kCBLReplicationStopped) {
        [self logFormat: @"Replication Stopped"];
+       NSDate *methodFinish = [NSDate date];
+       NSTimeInterval executionTime = [methodFinish timeIntervalSinceDate:start] * 1000;
+       [self logFormat:@"Test %@: Time to push replicate %d documents with size %dB, total latency %.2f: %.2f",[self class], kNumberOfDocuments, kSizeofDocument, executionTime, executionTime/kNumberOfDocuments];
        self.running = NO;
    }
-    
 }
-
 
 - (void) setUp {
     [super setUp];
+    self.heartbeatInterval = 1.0;
     
-    NSMutableData* utf8 = [NSMutableData dataWithLength: kSizeofDocument];
-    memset(utf8.mutableBytes, '1', utf8.length);
-    NSString* str = [[NSString alloc] initWithData: utf8 encoding: NSUTF8StringEncoding];
-    NSDictionary* props = @{@"k": str};
+    NSDictionary* testCaseConfig = [[BeeTest config] objectForKey:NSStringFromClass([self class])];
+    kNumberOfDocuments = [[testCaseConfig objectForKey:@"number_of_documents"] intValue];
+    kSizeofDocument = [[testCaseConfig  objectForKey:@"size_of_document"] intValue];
+    syncGatewayUrl = [testCaseConfig  objectForKey:@"sync_gateway_url"];
     
-    [self.database inTransaction:^BOOL{
-        for (int j = 0; j < kNumberOfDocuments; j++) {
-            @autoreleasepool {
-                CBLDocument* doc = [self.database createDocument];
-                NSError* error;
-                if (![doc putProperties: props error: &error]) {
-                    [self logFormat: @"!!! Failed to create doc %@", props];
-                    self.error = error;
+    [self logFormat: @"Starting Test %@ - numberOfDocuments %d, sizeofAttachment %d, to Sync_gateway %@",[self class], kNumberOfDocuments, kSizeofDocument,syncGatewayUrl];
+    
+    @autoreleasepool {
+        NSMutableData* utf8 = [NSMutableData dataWithLength: kSizeofDocument];
+        memset(utf8.mutableBytes, '1', utf8.length);
+        NSString* str = [[NSString alloc] initWithData: utf8 encoding: NSUTF8StringEncoding];
+        NSDictionary* props = @{@"k": str};
+        
+        [self.database inTransaction:^BOOL{
+            for (int j = 0; j < kNumberOfDocuments; j++) {
+                @autoreleasepool {
+                    CBLDocument* doc = [self.database createDocument];
+                    NSError* error;
+                    if (![doc putProperties: props error: &error]) {
+                        [self logFormat: @"!!! Failed to create doc %@", props];
+                        self.error = error;
+                    }
                 }
             }
-        }
-        return YES;
-    }];
-
+            return YES;
+        }];
+    }
     
-    NSURL *syncGateway  = [NSURL URLWithString:@"http://localhost:4984/db"];
-    
-    self.push = [self.database createPushReplication: syncGateway];
-    [self logFormat: @"Start Replication: Push"];
-
-    // Start measuring time from here
-    _pushStartTime = [NSDate date];
-    
-    [self.push start];
-
-    NSNotificationCenter* nctr = [NSNotificationCenter defaultCenter];
-    [nctr addObserver: self selector: @selector(replicationChanged:)
-                 name: kCBLReplicationChangeNotification object: self.push];
-    
+    @autoreleasepool {
+        NSURL *syncGateway  = [NSURL URLWithString:syncGatewayUrl];
+        self.push = [self.database createPushReplication: syncGateway];
+        [self logFormat: @"Starting Push Replication"];
+        
+        // Start measuring time from here
+        start = [NSDate date];
+        
+        [self.push start];
+        
+        NSNotificationCenter* nctr = [NSNotificationCenter defaultCenter];
+        [nctr addObserver: self selector: @selector(replicationChanged:)
+                     name: kCBLReplicationChangeNotification object: self.push];
+    }
 }
-
 
 @end
